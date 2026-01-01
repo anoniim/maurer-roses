99cf2d235e7635816926932f64ee97b89a476f79
Marcel Bro <marcel.broz@gmail.com>
2026-01-01 22:15:28 +0000

    New formats for printing in high-resolution

--- src/main/kotlin/net/solvetheriddle/openrndr/maurer/ui/Customizations.kt
+++ src/main/kotlin/net/solvetheriddle/openrndr/maurer/ui/Customizations.kt
@@ -23,12 +23,20 @@ enum class Display(val width: Int, val height: Int, val xPosition: Int, val yPos
     LG_ULTRAWIDE(2560, 1550, 1280),
     LG_ULTRAWIDE_LEFT(2560, 1550, 0),
     LG_SQUARE_LEFT(1080, 1080, 100, (LG_ULTRAWIDE.height - 1080) / 2),
+    LG_SQUARE_RIGHT(LG_ULTRAWIDE.height, LG_ULTRAWIDE.height, 3840 - LG_ULTRAWIDE.height),
     LG_PORTRAIT_LEFT(630, 1305, 325, (LG_ULTRAWIDE.height - 1305) / 2),
     MACBOOK_AIR(1500, 997, 500),
+    MACBOOK_SQUARE(1100,1100,700),
+
     PRINT_A6(1485,1050,0),
-    MACBOOK_SQUARE(1100,1100,700), // for 10x10cm prints
-    PRINT_SQUARE_SMALL(1536,1536,0), // for 10x10cm prints
-    PRINT_SQUARE_LARGE(8192,8192,0), // for 60x60cm prints
+    PRINT_SQUARE_SMALL(1536,1536,0), // 10x10cm prints
+
+    PRINT_60CM_SQUARE_PRINT(17008,17008,0), // 60x60cm with 720PPI
+    PRINT_20X60CM_PRINT(17008,17008,0), // 60x60cm with 720PPI
+
+    PRINT_A1_PORTRAIT(16_838, 23_839, 0), // 594 x 841 mm with 720PPI
+    PRINT_A2_PORTRAIT(11_906, 16_838, 0), // 420 x 594 mm with 720PPI
+    PRINT_A3_PORTRAIT(8_416, 11_906, 0), // 297 x 420 mm with 720PPI
     ;
     object FULLSCREEN: SketchSizeConfig
 }
